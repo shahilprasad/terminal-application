ca0da1e83bb1d04490df71c1494101445b36cbed
shahilprasad <shahil.prasad17s@gmail.com>
2023-10-23 20:48:09 +1100

    Main user interface printed and input from user promoted correctly.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-# Set up of github repo
+
+# Main menu for application
+def main():
+        print("-- Horse Stable Management App --")
+        print("1. Add a horse")
+        print("2. Remove a horse")
+        print("3. Update horse details")
+        print("4. View horse details")
+        print("5. Add race results")
+        print("6. View race logs")
+        print("7. Exit app")
+
+        choice = input("Enter your choice: ")
+
+main()
