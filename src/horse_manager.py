1ed535e99b20af7a3e7ef3e605e1e090987cec13
shahilprasad <shahil.prasad17s@gmail.com>
2023-10-25 11:44:23 +1100

    Added file.truncate() to race logs and horse manager as data was not being stored to the json file correctly.

--- src/horse_manager.py
+++ src/horse_manager.py
@@ -10,16 +10,20 @@ def add_horse():
     diet = input("Enter horse diet: ")
     new_horse = {"Name": name, "Age": age, "Health": health, "Diet": diet}
 
+# Validate the data entry from the user to ensure that it matches
+# against the specified schema, else return error message
     if validate_horse(new_horse):
         with open('data.json', 'r+') as file:
             data = json.load(file)
             data['horses'].append(new_horse)
             file.seek(0)
+            file.truncate()
             json.dump(data, file)
     else:
         print("Invalid horse data")
 
-# Remove horse from the database
+# Remove a horse from the database after checking the database
+# for a matching horse name, else return error message
 def remove_horse():
     horse_name = input("Enter the name of the horse to remove: ")
     with open('data.json', 'r+') as file:
@@ -29,12 +33,14 @@ def remove_horse():
             if horse['Name'] == horse_name:
                 horses.remove(horse)
                 file.seek(0)
+                file.truncate()
                 json.dump(data, file)
                 print(f"Horse {horse_name} removed successfully!")
                 return
         print(f"No horse found with the name {horse_name}.")
 
-# Proide option to update horse details in the database
+# Provide option to update horse details in the database
+# which will overwrite existing informatiom if match found
 def update_horse_details():
     horse_name = input("Enter the name of the horse to update: ")
     with open('data.json', 'r+') as file:
@@ -54,6 +60,7 @@ def update_horse_details():
                 if diet: horse['Diet'] = diet
                 
                 file.seek(0)
+                file.truncate()
                 json.dump(data, file)
                 print(f"Horse {horse_name} details updated successfully!")
                 return
--- src/race_logs_manager.py
+++ src/race_logs_manager.py
@@ -9,16 +9,19 @@ def add_race_log():
     race_result = input("Enter race result: ")
     new_log = {"HorseName": horse_name, "RaceDate": race_date, "RaceResult": race_result}
 
+# Validate the data entry from the user to ensure that it matches
+# against the specified schema, else return error message
     if validate_race_log(new_log):
         with open('data.json', 'r+') as file:
             data = json.load(file)
             data['race_logs'].append(new_log)
             file.seek(0)
+            file.truncate()
             json.dump(data, file)
     else:
         print("Invalid race log")
 
-# Display race logs to user from json file
+# Display race logs to user from the json file database
 def view_race_logs():
     with open('data.json', 'r') as file:
         data = json.load(file)
