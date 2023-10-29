f36553acab0c4ef91002bb2d210757de34f11341
shahilprasad <shahil.prasad17s@gmail.com>
2023-10-29 16:43:06 +1100

    Created script  which when run installs required python packages from requirements.txt and also sets up virtual environment for the user.

---
+++ src/run_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Starting setup for Horse Stable Management App..."
+
+#Creating virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Install required python packages 
+echo "Installing required packages..."
+python3 -m pip install -r requirements.txt
+echo "Setup completed successfully!"
+
+echo "To run the app, please enter the following: bash run_setup.sh"
